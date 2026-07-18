8a8d2600db4114f1fd088a81368e87c13da551bb
Hendry Varela <s1res0s6321@gmail.com>
2026-07-18 01:32:59 -0400

    fix: descomentar EnsureFrontendRequestsAreStateful para autenticar Axios de Inertia en API

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Illuminate\Http\Middleware\HandleCors::class, // <--- ESTA LÍNEA DEBE ESTAR AQUÍ
